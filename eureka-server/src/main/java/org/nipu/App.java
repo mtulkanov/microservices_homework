1d4db01ea49d3c6e1456d43f9a6422b431771461
Mike <mtulkanov@yandex.ru>
2019-11-15 23:58:19 +0400

    Remove spring boot starter parent

--- eureka-server/src/main/java/org/nipu/App.java
+++ eureka-server/src/main/java/org/nipu/App.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
-@SpringBootApplication
+@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration.class})
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class);
