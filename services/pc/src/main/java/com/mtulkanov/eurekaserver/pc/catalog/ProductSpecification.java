2aabbbab0a974e6a16882774437b7dc1f7bb78ae
Mike <mtulkanov@yandex.ru>
2019-12-10 21:19:34 +0400

    change dependency inheritance

--- services/pc/src/main/java/com/mtulkanov/eurekaserver/pc/catalog/ProductSpecification.java
+++ services/pc/src/main/java/com/mtulkanov/eurekaserver/pc/catalog/ProductSpecification.java
@@ -6,7 +6,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
 @Data
-class ProductSpecification {
+public class ProductSpecification {
     @Id
     private String id;
     private String name;
