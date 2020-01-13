36a805286de964765062df19ee2b98cb33ee5971
Mikhail Tiulkanov <Mikhail_Tiulkanov@epam.com>
2020-01-13 10:19:43 +0400

    Fix autowiring

--- services/po/src/test/groovy/com/mtulkanov/po/ApplicationContextTest.groovy
+++ services/po/src/test/groovy/com/mtulkanov/po/ApplicationContextTest.groovy
@@ -1,5 +1,6 @@
 package com.mtulkanov.po
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.ApplicationContext
 import spock.lang.Specification
@@ -7,6 +8,7 @@ import spock.lang.Specification
 @SpringBootTest
 class ApplicationContextTest extends Specification {
 
+    @Autowired
     ApplicationContext context
 
     def 'context loads'() {
